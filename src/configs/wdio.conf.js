4847fa0d1298dec599bb39e90fc564ec4c3905fe
korolevichlisa <korolevich.liza@gmail.com>
2024-09-15 14:18:29 +0300

    Was added html reporter

--- src/configs/wdio.conf.js
+++ src/configs/wdio.conf.js
@@ -150,7 +150,19 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['spec',
+      ['html-nice',{
+        outputDir: './reports/html-reports/',
+        filename: 'report.html',
+        reportTitle: 'Test Report Title',
+        linkScreenshots: true,
+            //to show the report in a browser when done
+        showInBrowser: true,
+        collapseTests: false,
+            //to turn on screenshots after every test
+        useOnAfterCommandForScreenshot: false
+      }]
+    ],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
--- src/po/pages/signIn.page.js
+++ src/po/pages/signIn.page.js
@@ -3,7 +3,8 @@ import { StartPage } from "./start.page.js"
 const loginPageLocators = {
     username: 'input[data-testid="username"]',
     password: 'input[data-testid="password"]',
-    loginBtn: '#login-submit'
+    loginBtn: '#login-submit',
+    mfaDismiss:'#mfa-promote-dismiss'
 }
 
 export class SignIn extends StartPage{
@@ -19,13 +20,22 @@ export class SignIn extends StartPage{
     get loginSubmitBtn() {
         return $(loginPageLocators.loginBtn)
     }
+
+    get mfaDismiss(){
+        return $(loginPageLocators.mfaDismiss)
+    }
    
     async signIn() {
         await this.usernameInput.setValue(process.env.user_email)
         await this.loginSubmitBtn.click()
         await this.passwordInput.waitForDisplayed({timeout: 3500})
         await this.passwordInput.setValue(process.env.user_pass)
-        await this.loginSubmitBtn.click()        
+        await this.loginSubmitBtn.click()
+        // await this.passwordInput.waitForDisplayed({timeout: 6500})
+
+        if(await this.mfaDismiss.isExisting()){
+            await this.mfaDismiss.click()
+        }   
     }
 
     open() {
